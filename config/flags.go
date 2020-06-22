d934e7c95ae0bb700aa2dba04f75acec78a033c9
findkim <kngo@hashicorp.com>
2020-06-22 15:02:12 -0500

    Add custom flag type for multiple config file/dir options

---
+++ config/flags.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"flag"
+	"strings"
+)
+
+var _ flag.Value = (*FlagAppendSliceValue)(nil)
+
+// FlagAppendSliceValue implements the flag.Value interface and allows multiple
+// calls to the same variable to append a list.
+type FlagAppendSliceValue []string
+
+func (s *FlagAppendSliceValue) String() string {
+	return strings.Join(*s, ",")
+}
+
+func (s *FlagAppendSliceValue) Set(value string) error {
+	if *s == nil {
+		*s = make([]string, 0, 1)
+	}
+
+	*s = append(*s, value)
+	return nil
+}
