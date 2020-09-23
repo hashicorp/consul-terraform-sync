74699a122b5a8784d39595f268738327df99561d
Lorna Song <lorna@hashicorp.com>
2020-09-23 14:19:36 -0400

    Retry terraform init & apply on fail
    - Initial pass at retry for tech preview with exponential backoff with a random delay
    - Add *Rand to each worker rather than sharing global seed since workers will need to to generate random numbers concurrently
    - Default to retry 2x (in addition to first execution) for now. No retries for testing

--- driver/task.go
+++ driver/task.go
@@ -2,8 +2,13 @@ package driver
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
 
 	"github.com/hashicorp/consul-nia/client"
+	"github.com/pkg/errors"
 )
 
 // Service contains service configuration information
@@ -33,23 +38,78 @@ type Task struct {
 type worker struct {
 	client client.Client
 	task   Task
+	random *rand.Rand
 
+	retry  int
 	inited bool
 }
 
 func (w *worker) init(ctx context.Context) error {
-	if err := w.client.Init(ctx); err != nil {
-		// TODO: retry strategy
+	err := withRetry(func() error {
+		return w.client.Init(ctx)
+	}, fmt.Sprintf("Init %s", w.task.Name), w.random, w.retry)
+	if err != nil {
 		return err
 	}
+
 	w.inited = true
 	return nil
 }
 
 func (w *worker) apply(ctx context.Context) error {
-	if err := w.client.Apply(ctx); err != nil {
-		// TODO: retry strategy
-		return err
+	return withRetry(func() error {
+		return w.client.Apply(ctx)
+	}, fmt.Sprintf("Apply %s", w.task.Name), w.random, w.retry)
+}
+
+// withRetry calls a function with exponential retry with a random delay. First
+// call also has random delay.
+func withRetry(fxn func() error, desc string, random *rand.Rand, retry int) error {
+	count := retry + 1
+	var errs error
+
+	attempt := 0
+	wait := waitTime(attempt, random)
+	interval := time.NewTicker(time.Duration(wait))
+	defer interval.Stop()
+
+	for {
+		select {
+		case <-interval.C:
+			attempt++
+			if attempt > 1 {
+				log.Printf("[WARN]: retrying '%s'. attempt #%d", desc, attempt)
+			}
+			err := fxn()
+			if err == nil {
+				return nil
+			}
+
+			err = fmt.Errorf("attempt #%d failed '%s'", attempt, err)
+
+			if errs == nil {
+				errs = err
+			} else {
+				errs = errors.Wrap(errs, err.Error())
+			}
+
+			wait := waitTime(attempt, random)
+			interval = time.NewTicker(time.Duration(wait))
+		}
+		if attempt >= count {
+			return errs
+		}
 	}
-	return nil
+}
+
+// waitTime calculates the wait time based off the attempt number based off
+// exponential backoff with a random delay.
+func waitTime(attempt int, random *rand.Rand) int {
+	a := float64(attempt)
+	baseTimeSeconds := a * a
+	nextTimeSeconds := (a + 1) * (a + 1)
+	delayRange := (nextTimeSeconds - baseTimeSeconds) / 2
+	delay := random.Float64() * delayRange
+	total := (baseTimeSeconds + delay) * float64(time.Second)
+	return int(total)
 }
--- driver/task_test.go
+++ driver/task_test.go
@@ -3,7 +3,10 @@ package driver
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	mocks "github.com/hashicorp/consul-nia/mocks/client"
 	"github.com/stretchr/testify/assert"
@@ -32,7 +35,10 @@ func TestWorkerInit(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			c := new(mocks.Client)
 			c.On("Init", mock.Anything).Return(tc.initErr)
-			w := worker{client: c}
+			w := worker{
+				client: c,
+				random: rand.New(rand.NewSource(1)),
+			}
 
 			err := w.init(ctx)
 			if tc.initErr != nil {
@@ -68,7 +74,10 @@ func TestWorkerApply(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			c := new(mocks.Client)
 			c.On("Apply", mock.Anything).Return(tc.applyErr)
-			w := worker{client: c}
+			w := worker{
+				client: c,
+				random: rand.New(rand.NewSource(1)),
+			}
 
 			err := w.apply(ctx)
 			if tc.applyErr != nil {
@@ -76,6 +85,105 @@ func TestWorkerApply(t *testing.T) {
 				return
 			}
 			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestWithRetry(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name      string
+		retry     int
+		successOn int
+		expected  error
+	}{
+		{
+			"happy path: retry once, success on retry (2)",
+			1,
+			2,
+			nil,
+		},
+		{
+			"no success on retries: retry once",
+			1,
+			100,
+			errors.New("attempt #2 failed 'error on 2': attempt #1 failed 'error on 1'"),
+		},
+		{
+			"happy path: no retry",
+			0,
+			1,
+			nil,
+		},
+		{
+			"no retry, no success",
+			0,
+			100,
+			errors.New("attempt #1 failed 'error on 1'"),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			// set up fake function
+			count := 0
+			fxn := func() error {
+				count++
+				if count == tc.successOn {
+					return nil
+				}
+				return fmt.Errorf("error on %d", count)
+			}
+
+			random := rand.New(rand.NewSource(1))
+			err := withRetry(fxn, "test fxn", random, tc.retry)
+			if tc.expected == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.Equal(t, tc.expected.Error(), err.Error())
+			}
+		})
+	}
+}
+
+func TestWaitTime(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name      string
+		attempt   int
+		minReturn float64
+		maxReturn float64
+	}{
+		{
+			"first attempt",
+			0,
+			0,
+			0.5,
+		},
+		{
+			"second attempt",
+			1,
+			1,
+			2.5,
+		},
+		{
+			"third attempt",
+			2,
+			4,
+			6.5,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			random := rand.New(rand.NewSource(1))
+			a := waitTime(tc.attempt, random)
+
+			actual := float64(a) / float64(time.Second)
+			assert.GreaterOrEqual(t, actual, tc.minReturn)
+			assert.LessOrEqual(t, actual, tc.maxReturn)
 		})
 	}
 }
