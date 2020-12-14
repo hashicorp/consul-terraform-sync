684bd4c93dba4265964a9fde9614d4d73da0646d
findkim <kngo@hashicorp.com>
2020-12-14 10:55:43 -0600

    Update more tests

--- driver/terraform_provider.go
+++ driver/terraform_provider.go
@@ -2,13 +2,19 @@ package driver
 
 import "github.com/hashicorp/consul-terraform-sync/templates/hcltmpl"
 
+// TerraformProviderBlock contains provider arguments and environment variables
+// for the Terraform provider.
 type TerraformProviderBlock struct {
 	block hcltmpl.NamedBlock
 	env   map[string]string
 }
 
+// TerraformProviderBlocks are a list of providers and their arguments and env
 type TerraformProviderBlocks []TerraformProviderBlock
 
+// NewTerraformProviderBlocks creates a new list of provider blocks with the
+// environment variables separated from provider arguments from the base hcl
+// block.
 func NewTerraformProviderBlocks(blocks []hcltmpl.NamedBlock) TerraformProviderBlocks {
 	providers := make([]TerraformProviderBlock, len(blocks))
 	for i, p := range blocks {
@@ -17,6 +23,8 @@ func NewTerraformProviderBlocks(blocks []hcltmpl.NamedBlock) TerraformProviderBl
 	return providers
 }
 
+// NewTerraformProviderBlock creates a provider block with the environment
+// variables separated from provider arguments from the base hcl block.
 func NewTerraformProviderBlock(b hcltmpl.NamedBlock) TerraformProviderBlock {
 	env := make(map[string]string)
 
@@ -37,18 +45,25 @@ func NewTerraformProviderBlock(b hcltmpl.NamedBlock) TerraformProviderBlock {
 	}
 }
 
+// Name returns the name of the provider. This is the label of the HCL named
+// block.
 func (p TerraformProviderBlock) Name() string {
 	return p.block.Name
 }
 
+// ProviderBlock returns the arguments for the Terraform provider block.
 func (p TerraformProviderBlock) ProviderBlock() hcltmpl.NamedBlock {
 	return p.block
 }
 
+// Env returns the configured environment variables for the Terraform provider.
+// These values are set for the task workspace and are not written to any
+// generated Terraform configuration file.
 func (p TerraformProviderBlock) Env() map[string]string {
 	return p.env
 }
 
+// ProviderBlocks returns a list of the provider blocks.
 func (p TerraformProviderBlocks) ProviderBlocks() []hcltmpl.NamedBlock {
 	blocks := make([]hcltmpl.NamedBlock, len(p))
 	for i, b := range p {
@@ -57,6 +72,7 @@ func (p TerraformProviderBlocks) ProviderBlocks() []hcltmpl.NamedBlock {
 	return blocks
 }
 
+// Env returns a merged map of environment variables across all providers.
 func (p TerraformProviderBlocks) Env() map[string]string {
 	env := make(map[string]string)
 	for _, b := range p {
