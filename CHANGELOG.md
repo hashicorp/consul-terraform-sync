354ce7a8aa7e434e0c74e3d9d0ecc1e33235b443
hashicorp-ci <hashicorp-ci@users.noreply.github.com>
2021-03-29 18:08:41 +0000

    Release v0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## UNRELEASED
+## 0.1.0 (March 29, 2021)
 
 BUG FIXES:
 * Fix Task Status API response which was incorrectly returning empty providers and services information when requesting a task with no event data. [[GH-219](https://github.com/hashicorp/consul-terraform-sync/pull/219)]
--- version/version.go
+++ version/version.go
@@ -26,7 +26,7 @@ var (
 	// VersionPrerelease is a pre-release marker for the version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "beta"
+	VersionPrerelease = ""
 )
 
 // GetHumanVersion composes the parts of the version in a way that's suitable
