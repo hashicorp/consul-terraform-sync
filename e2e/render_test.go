29eae43236188108bddc264742859d4aac783f4d
John Eikenberry <jae@zhar.net>
2021-04-02 18:22:11 -0700

    add test for services render race
    Tests #239

---
+++ e2e/render_test.go
@@ -0,0 +1,79 @@
+// +build e2e
+
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/hashicorp/consul-terraform-sync/config"
+	"github.com/hashicorp/consul-terraform-sync/controller"
+	"github.com/hashicorp/consul-terraform-sync/event"
+	"github.com/hashicorp/consul-terraform-sync/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+// issues GH-236 and GH-239
+func TestServicesRenderRace(t *testing.T) {
+	const NumberOfServices = 100
+
+	srv := testutils.NewTestConsulServer(t,
+		testutils.TestConsulServerConfig{HTTPSRelPath: "../testutils"})
+	defer srv.Stop()
+
+	tempDir := t.Name()
+	cleanup := testutils.MakeTempDir(t, tempDir)
+	//_ = cleanup
+	defer cleanup()
+
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	defer ctxCancel()
+
+	// seed server with test data
+	testServices := testutils.TestServices(NumberOfServices)
+	testutils.AddServices(t, srv, testServices)
+
+	// config
+	conf := config.DefaultConfig()
+	conf.BufferPeriod.Enabled = config.Bool(false)
+	serviceNames := make([]string, NumberOfServices)
+	for i, s := range testServices {
+		serviceNames[i] = s.Name
+	}
+	conf.Tasks = &config.TaskConfigs{
+		&config.TaskConfig{
+			Name:     config.String("serv_rend_race_task"),
+			Source:   config.String("../../test_modules/null_resource"),
+			Services: serviceNames,
+		}}
+	conf.Consul.Address = config.String(srv.HTTPAddr)
+	conf.Finalize()
+	conf.Driver.Terraform.WorkingDir = config.String(tempDir)
+	path, err := filepath.Abs("./")
+	require.NoError(t, err)
+	conf.Driver.Terraform.Path = config.String(path)
+
+	// download terraform
+	err = controller.InstallDriver(ctx, conf)
+	require.NoError(t, err)
+
+	// run controller
+	ctrl, err := controller.NewReadWrite(conf, event.NewStore())
+	rwCtrl := ctrl.(*controller.ReadWrite)
+	require.NoError(t, err)
+	_, err = rwCtrl.Init(ctx)
+	require.NoError(t, err)
+	err = rwCtrl.Once(ctx)
+	require.NoError(t, err)
+
+	// veryify results
+	tfvarsFile := filepath.Join(tempDir, "serv_rend_race_task", "terraform.tfvars")
+	data, err := os.ReadFile(tfvarsFile)
+	require.NoError(t, err)
+	// 'svc_name_' is unique per service and much easier to count than
+	// parsing the file and counting the real entries
+	require.Equal(t, NumberOfServices, bytes.Count(data, []byte("svc_name_")))
+}
