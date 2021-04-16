f1cbf6bb14577323152fa7e34922c5dbb781d9fe
Lorna Song <lorna@hashicorp.com>
2021-04-16 12:58:35 -0400

    Update config decoding to support condition config
    As is, configuration decoding will error out when a condition is configured:
    
    `'task[0].condition' expected type 'config.ConditionConfig', got
    'map[string]interface {}'`
    
    Changes:
     - Add a decode hook to identify config.ConditionConfig type and decode the
     map[string]interface{} into an implementation of ConditionConfig
     - Tests for different condition config edge-cases

--- config/condition.go
+++ config/condition.go
@@ -1,7 +1,14 @@
 package config
 
 import (
+	"fmt"
+	"log"
 	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/hashicorp/consul/lib/decode"
+	"github.com/mitchellh/mapstructure"
 )
 
 // ConditionConfig configures a condition on a task to define the condition on
@@ -25,4 +32,82 @@ func DefaultConditionConfig() ConditionConfig {
 // implementation i.e. isNil(ConditionConfig), servicesConditionConfig == nil
 func isNil(c ConditionConfig) bool {
 	return c == nil || reflect.ValueOf(c).IsNil()
+}
+
+// conditionToTypeFunc is a decode hook function to decode a ConditionConfig
+// into a specific condition implementation structures. Used when decoding
+// cts config overall.
+func conditionToTypeFunc() mapstructure.DecodeHookFunc {
+	return func(
+		f reflect.Type,
+		t reflect.Type,
+		data interface{}) (interface{}, error) {
+		// identify if parsing a ConditionConfig
+		var i ConditionConfig
+		if t != reflect.TypeOf(&i).Elem() {
+			return data, nil
+		}
+
+		// abstract conditions map out depending on hcl vs. json formatting
+		// data hcl ex: [map[catalog-services:[map[regexp:.*]]]]
+		// data json ex: map[catalog-services:map[regexp:.*]]
+		var conditions map[string]interface{}
+		if hcl, ok := data.([]map[string]interface{}); ok {
+			if len(hcl) != 1 {
+				return nil, fmt.Errorf("expected only one item in hcl "+
+					"condition but got %d: %v", len(hcl), data)
+			}
+			conditions = hcl[0]
+		}
+		if json, ok := data.(map[string]interface{}); ok {
+			conditions = json
+		}
+
+		if c, ok := conditions[catalogServicesConditionType]; ok {
+			var config CatalogServicesConditionConfig
+			return decodeConditionToType(c, &config)
+		}
+		if c, ok := conditions[servicesConditionType]; ok {
+			var config ServicesConditionConfig
+			return decodeConditionToType(c, &config)
+		}
+
+		return nil, fmt.Errorf("unsupported condition type: %v", data)
+	}
+}
+
+// decodeConditionToType is used by the overall config mapstructure decode hook
+// conditionToTypeFunc in order to convert ConditionConfig in the form
+// of an interface into an implementation
+func decodeConditionToType(data interface{}, condition ConditionConfig) (ConditionConfig, error) {
+	var md mapstructure.Metadata
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			decode.HookWeakDecodeFromSlice,
+		),
+		WeaklyTypedInput: true,
+		ErrorUnused:      false,
+		Metadata:         &md,
+		Result:           &condition,
+	})
+	if err != nil {
+		log.Printf("[ERR] (config) condition mapstructure decoder creation"+
+			"failed: %s", err)
+		return nil, err
+	}
+
+	if err := decoder.Decode(data); err != nil {
+		log.Printf("[ERR] (config) condition mapstructure decode failed: %s",
+			err)
+		return nil, err
+	}
+
+	if len(md.Unused) > 0 {
+		sort.Strings(md.Unused)
+		err := fmt.Errorf("invalid keys: %s", strings.Join(md.Unused, ", "))
+		log.Printf("[ERR] (config) condition %s", err.Error())
+		return nil, err
+	}
+
+	return condition, nil
 }
