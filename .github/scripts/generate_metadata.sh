c810b9ec5a8d4ab97460c9b5c59645b088047c47
Natalie Serebryakova <99>
2021-08-24 00:35:59 -0700

    fix version

--- .github/scripts/generate_metadata.sh
+++ .github/scripts/generate_metadata.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 declare -r org="hashicorp"
 declare -r product="consul-terraform-sync"
 
-#if ! product_version="$(make version)"; then
-#  echo "Error running 'make version'"
-#  echo "${product_version}"
-#  exit 1
-#fi
+if ! product_version="$(make version)"; then
+  echo "Error running 'make version'"
+  echo "${product_version}"
+  exit 1
+fi
 
 if ! git_sha="$(git rev-parse HEAD)"; then
   echo "Unable to determine git sha for this commit"
@@ -21,7 +21,7 @@ cat <<-EOM
   "org": "${org}",
   "product": "${product}",
   "sha": "${git_sha}",
-  "version": "v0.2.1",
+  "version": "${product_version}",
   "buildworkflowid" : "${GITHUB_RUN_ID}"
 }
 EOM
