9590fcac1fee790fc4ee3e9e4b16e64625d5b703
Sarah <sthompson@hashicorp.com>
2021-08-25 19:22:44 +0100

    Adding docker buildx to build the multi arch images properly Some updates to build.yml to make sure we're uplaoding all artifacts (rpms + debs included) Build arm5 and arm6

--- packaging/docker/docker-entrypoint.sh
+++ packaging/docker/docker-entrypoint.sh
@@ -9,27 +9,24 @@ set -e
 # If the user is trying to run consul-terraform-sync directly with some arguments,
 # then pass them to consul-terraform-sync.
 # On alpine /bin/sh is busybox which supports the bashism below.
-if [ "${1:0:1}" = '-' ]
-then
-    set -- /bin/consul-terraform-sync "$@"
+if [ "${1:0:1}" = '-' ]; then
+	set -- /bin/consul-terraform-sync "$@"
 fi
 
 # If user is trying to run consul-terraform-sync with no arguments (daemon-mode),
 # docker will run '/bin/sh -c /bin/${NAME}'. Check for the full command since
 # running 'bin/sh' is a common pattern
-if [ "$*" = '/bin/sh -c /bin/${NAME}' ]
-then
-    set -- /bin/consul-terraform-sync
+if [ "$*" = '/bin/sh -c /bin/${NAME}' ]; then
+	set -- /bin/consul-terraform-sync
 fi
 
 # Matches VOLUME in the Dockerfile, for importing config files into image
 CTS_CONFIG_DIR=/consul-terraform-sync/config
 
 # Set the configuration directory
-if [ "$1" = '/bin/consul-terraform-sync' ]
-then
-  shift
-  set -- /bin/consul-terraform-sync -config-dir="$CTS_CONFIG_DIR" "$@"
+if [ "$1" = '/bin/consul-terraform-sync' ]; then
+	shift
+	set -- /bin/consul-terraform-sync -config-dir="$CTS_CONFIG_DIR" "$@"
 fi
 
 exec "$@"
