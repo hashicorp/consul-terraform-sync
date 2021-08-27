f04844f87919c223816a089b519af86c352e99b8
Lorna Song <lorna@hashicorp.com>
2021-08-27 16:37:38 -0400

    Add SuppressNotifier for scheduled tasks
    For schedule condition, we do not want to notify the watcher of dependency
    changes because the schedule task will manage it's own execution based on
    schedule. This prevents the schedule task's dependencies from triggering
    dynamic tasks.
    
    SuppressNotifier:
     - Does not notify watcher on dependency changes
     - Still keeps the template up-to-date with dependency changes
     - Notifies the watcher only for once-mode

---
+++ templates/tftmpl/notifier/suppress_notification.go
@@ -0,0 +1,70 @@
+package notifier
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/consul-terraform-sync/logging"
+	"github.com/hashicorp/consul-terraform-sync/templates"
+)
+
+const supSubsystemName = "suppress"
+
+// SuppressNotification is a custom notifier expected to be used for tasks that
+// are not triggered by the hcat.watcher and are instead triggered by a
+// separate process.
+//
+// The watcher is shared by all dynamic tasks as they wait on watcher for
+// dependency changes. On a dependency change, all dynamic tasks are triggered
+// to re-render templates and terraform-apply if there are changes to the
+// template
+//
+// Non-dynamic tasks, such as scheduled tasks, do not wait on the watcher and
+// therefore should use a SuppressNotification notify to avoid notifying the
+// watcher because it will un-necessarily cause all dynamic tasks to trigger.
+type SuppressNotification struct {
+	templates.Template
+	logger logging.Logger
+
+	// count all dependencies needed to complete once-mode
+	once     bool
+	depTotal int
+	counter  int
+}
+
+// NewSuppressNotification creates a new SuppressNotification notifier.
+// serviceCount parameter: the number of services the task is configured with
+func NewSuppressNotification(tmpl templates.Template, serviceCount int) *SuppressNotification {
+	return &SuppressNotification{
+		Template: tmpl,
+		depTotal: serviceCount,
+		logger:   logging.Global().Named(logSystemName).Named(supSubsystemName),
+	}
+}
+
+// Notify suppresses all notifications on any dependency changes. However,
+// it will pass the latest dependency information to the template until the
+// task is triggered by another means.
+//
+// Once-mode requires a notification when all dependencies are received in order
+// to trigger CTS. It will hang otherwise.
+func (n *SuppressNotification) Notify(d interface{}) (notify bool) {
+	n.logger.Debug("received dependency change", "dependency_type", fmt.Sprintf("%T", d))
+
+	notify = false
+
+	if !n.once {
+		n.counter++
+		// after all dependencies are received, notify so once-mode can complete
+		if n.counter >= n.depTotal {
+			n.logger.Debug("notify once-mode complete")
+			n.once = true
+			notify = true
+		}
+	}
+
+	// keep the template up-to-date with latest dependency information even
+	// if we do not notify watcher.
+	n.Template.Notify(d)
+
+	return notify
+}
---
+++ templates/tftmpl/notifier/suppress_notification_test.go
@@ -0,0 +1,68 @@
+package notifier
+
+import (
+	"testing"
+
+	mocks "github.com/hashicorp/consul-terraform-sync/mocks/templates"
+	"github.com/hashicorp/hcat/dep"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func Test_SuppressNotification_Notify(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name     string
+		expected bool
+	}{
+		{
+			"happy path",
+			false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tmpl := new(mocks.Template)
+			tmpl.On("Notify", mock.Anything).Return(true)
+
+			n := NewSuppressNotification(tmpl, 2)
+			actual := n.Notify([]*dep.HealthService{})
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+
+	t.Run("once-mode", func(t *testing.T) {
+		// Test that notifier only notifies when once-mode completes
+
+		// Notifier in test will have two dependencies
+		// 1. receive 1st services dependency, no notification
+		// 2. receive 2nd services dependency, notify because once-mode complete
+		// 3. receive subsequent services dependencies, no more notifications
+		// now that once-mode is complete
+
+		tmpl := new(mocks.Template)
+		tmpl.On("Notify", mock.Anything).Return(true)
+		n := NewSuppressNotification(tmpl, 2)
+
+		// 1. first services dependency does not notify
+		notify := n.Notify([]*dep.HealthService{})
+		assert.False(t, notify, "first services dep should not have notified")
+		assert.False(t, n.once, "got 1/2 deps. once-mode should not be completed")
+		assert.Equal(t, 1, n.counter, "first services dep should be 1st dep")
+
+		// 2. second services dependency should notify
+		notify = n.Notify([]*dep.HealthService{})
+		assert.True(t, notify, "second service dep should have notified")
+		assert.True(t, n.once, "got 2/2 deps. once-mode should be completed")
+		assert.Equal(t, 2, n.counter, "second services dep should be 2nd dep")
+
+		// 3. subsequent services dependency do not notify
+		notify = n.Notify([]*dep.HealthService{})
+		assert.False(t, notify, "third services dep should not have notified")
+		assert.False(t, notify, "fourth services dep should not have notified")
+		assert.False(t, notify, "fifth services dep should not have notified")
+		assert.False(t, notify, "sixth services dep should not have notified")
+	})
+}
