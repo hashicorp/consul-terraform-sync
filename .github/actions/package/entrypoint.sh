c3093e56c02f8eef906adf7e572b939eb724841d
Sam Salisbury <samsalisbury@gmail.com>
2021-08-31 12:54:29 +0100

    Linux packaging (#364)
    * delete embedded package GH action
    
    * ci: use external hashicorp/package action
    
    * ci: packaging: add description field
    
    * ci: fix indentation in yaml
    
    * ci: packaging: remove cruft

--- .github/actions/package/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Creating template files..."
-INPUT_DEPENDS=$INPUT_RPM_DEPENDS /usr/local/bin/nfpm_template > ./nfpm_rpm_config.yaml
-INPUT_DEPENDS=$INPUT_DEB_DEPENDS /usr/local/bin/nfpm_template > ./nfpm_deb_config.yaml
-
-echo "Packaging..."
-/usr/local/bin/nfpm package -f ./nfpm_rpm_config.yaml -p rpm -t ./out/
-/usr/local/bin/nfpm package -f ./nfpm_deb_config.yaml -p deb -t ./out/
--- .github/actions/package/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/hashicorp/consul-terraform-sync/action/package
-
-go 1.16
