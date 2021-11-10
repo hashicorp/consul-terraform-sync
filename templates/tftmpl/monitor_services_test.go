14ad0f96e11badd2192304c78b9b1cd5942ff770
Lorna Song <lorna@hashicorp.com>
2021-11-10 15:25:54 -0500

    Update Services condition and source input with datacenter, namespaces, filter
    Update templating for condition "services" and source_input "services" to
    support new fields: datacenter, namespace, and filter
    
    Changes:
     - Pass new fields from configuration to templating
     - Update tmpl.ServicesMonitor with new fields
     - New tests for tmpl.ServicesMonitor and tmpl.ServicesCondition (embedded with
     tfmpl.ServicesMonitor but overrides a function)

---
+++ templates/tftmpl/monitor_services_test.go
@@ -0,0 +1,80 @@
+package tftmpl
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestServicesMonitor_appendTemplate(t *testing.T) {
+	testcases := []struct {
+		name string
+		c    *ServicesMonitor
+		exp  string
+	}{
+		{
+			"happy path",
+			&ServicesMonitor{
+				Regexp:     ".*",
+				Datacenter: "dc1",
+				Namespace:  "ns1",
+				Filter:     "filter",
+			},
+			`
+services = {
+{{- with $srv := servicesRegex "regexp=.*" "dc=dc1" "ns=ns1" "filter" }}
+  {{- range $s := $srv}}
+  "{{ joinStrings "." .ID .Node .Namespace .NodeDatacenter }}" = {
+{{ HCLService $s | indent 4 }}
+  },
+  {{- end}}
+{{- end}}
+}
+`,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			w := new(strings.Builder)
+			err := tc.c.appendTemplate(w)
+			require.NoError(t, err)
+			assert.Equal(t, tc.exp, w.String())
+		})
+	}
+}
+
+func TestServicesMonitor_hcatQuery(t *testing.T) {
+	testcase := []struct {
+		name string
+		c    *ServicesMonitor
+		exp  string
+	}{
+		{
+			"regexp only",
+			&ServicesMonitor{
+				Regexp: ".*",
+			},
+			`"regexp=.*"`,
+		},
+		{
+			"all_parameters",
+			&ServicesMonitor{
+				Regexp:     ".*",
+				Datacenter: "datacenter",
+				Namespace:  "namespace",
+				Filter:     "filter",
+			},
+			`"regexp=.*" "dc=datacenter" "ns=namespace" "filter"`,
+		},
+	}
+
+	for _, tc := range testcase {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := tc.c.hcatQuery()
+			assert.Equal(t, tc.exp, actual)
+		})
+	}
+}
