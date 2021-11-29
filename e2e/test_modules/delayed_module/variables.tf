3dd4c108f44b50c2234dc37ef7bc31da9ca3d02d
Melissa Kam <mkam@hashicorp.com>
2021-11-29 14:11:36 -0600

    End-to-end tests for deleting a task
    - Delete tests using task status API to confirm existence or deletion
    - Adds a module that waits 5s before completing to force a conflict

---
+++ e2e/test_modules/delayed_module/variables.tf
@@ -0,0 +1,29 @@
+variable "services" {
+  description = "Consul services monitored by Consul Terraform Sync"
+  type = map(
+    object({
+      id        = string
+      name      = string
+      address   = string
+      port      = number
+      meta      = map(string)
+      tags      = list(string)
+      namespace = string
+      status    = string
+
+      node                  = string
+      node_id               = string
+      node_address          = string
+      node_datacenter       = string
+      node_tagged_addresses = map(string)
+      node_meta             = map(string)
+    })
+  )
+}
+
+
+variable "delay" {
+  description = "duration of time to delay before completing"
+  type        = string
+  default     = "5s"
+}
