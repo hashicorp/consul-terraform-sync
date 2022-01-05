0089dc18b807fa60be699afc73cabeee27bde858
Melissa Kam <mkam@hashicorp.com>
2022-01-05 12:22:15 -0600

    Update tests and examples with TF v1.1

--- templates/tftmpl/testdata/main.tf
+++ templates/tftmpl/testdata/main.tf
@@ -8,11 +8,11 @@
 # Description: user description for task named 'test'
 
 terraform {
-  required_version = ">= 0.13.0, < 1.1.0"
+  required_version = ">= 0.13.0, < 1.2.0"
   required_providers {
     testProvider = {
       source  = "namespace/testProvider"
-      version = "1.0.0"
+      version = "1.1.0"
     }
   }
   backend "consul" {
