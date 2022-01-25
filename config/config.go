a35b0ebffa333be9f3ad68e7c568cee28e8b29f7
Kim Ngo <6362111+findkim@users.noreply.github.com>
2022-01-25 12:59:19 -0600

    Remove consul dep
    Consul is a large repo and CTS is only using it for HCL decoding
    purposes. This removes the dep and copies over 2 functions.
    
    This reduces the range of dependencies added into CTS by importing
    the whole consul project, and now reduced to go modules, "api"
    and "sdk/testutils"
    
    (cherry picked from commit f318bad87794d67623f334ae141596d2efc639c3)

--- config/config.go
+++ config/config.go
@@ -10,8 +10,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/hashicorp/consul-terraform-sync/internal/decode"
 	"github.com/hashicorp/consul-terraform-sync/templates/hcltmpl"
-	"github.com/hashicorp/consul/lib/decode"
 	"github.com/hashicorp/hcl"
 	"github.com/mitchellh/mapstructure"
 )
