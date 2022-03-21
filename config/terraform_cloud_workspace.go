06276759354c11e1f16e11a2e7af7d1fc3783feb
Melissa Kam <mkam@hashicorp.com>
2022-03-21 17:44:57 -0500

    Add new config for Terraform Cloud workspace
    Add enterprise-only configuration to set values for task-specific
    workspace values.

---
+++ config/terraform_cloud_workspace.go
@@ -0,0 +1,129 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/consul-terraform-sync/logging"
+)
+
+// TerraformCloudWorkspaceConfig is an enterprise-only configuration that controls
+// workspace attributes that are specific to a task.
+type TerraformCloudWorkspaceConfig struct {
+	ExecutionMode *string `mapstructure:"execution_mode"`
+	AgentPoolID   *string `mapstructure:"agent_pool_id"`
+	AgentPoolName *string `mapstructure:"agent_pool_name"`
+}
+
+// Copy returns a deep copy of this configuration.
+func (c *TerraformCloudWorkspaceConfig) Copy() *TerraformCloudWorkspaceConfig {
+	if c == nil {
+		return nil
+	}
+
+	var o TerraformCloudWorkspaceConfig
+	o.ExecutionMode = c.ExecutionMode
+	o.AgentPoolID = c.AgentPoolID
+	o.AgentPoolName = c.AgentPoolName
+
+	return &o
+}
+
+// Merge combines all values in this configuration with the values in the other
+// configuration, with values in the other configuration taking precedence.
+func (c *TerraformCloudWorkspaceConfig) Merge(o *TerraformCloudWorkspaceConfig) *TerraformCloudWorkspaceConfig {
+	if c == nil {
+		if o == nil {
+			return o
+		}
+		return o.Copy()
+	}
+
+	if o == nil {
+		return c.Copy()
+	}
+
+	r := c.Copy()
+
+	if o.ExecutionMode != nil {
+		r.ExecutionMode = StringCopy(o.ExecutionMode)
+	}
+
+	if o.AgentPoolID != nil {
+		r.AgentPoolID = StringCopy(o.AgentPoolID)
+	}
+
+	if o.AgentPoolName != nil {
+		r.AgentPoolName = StringCopy(o.AgentPoolName)
+	}
+
+	return r
+}
+
+// Finalize ensures that the receiver contains no nil pointers.
+func (c *TerraformCloudWorkspaceConfig) Finalize() {
+	if c == nil { // config is not required, return early
+		return
+	}
+
+	if c.ExecutionMode == nil {
+		c.ExecutionMode = String("remote")
+	}
+
+	if c.AgentPoolID == nil {
+		c.AgentPoolID = String("")
+	}
+
+	if c.AgentPoolName == nil {
+		c.AgentPoolName = String("")
+	}
+}
+
+// Validate validates the values of the configuration struct
+func (c *TerraformCloudWorkspaceConfig) Validate() error {
+	if c == nil { // config not required, return early
+		return nil
+	}
+
+	if StringVal(c.ExecutionMode) == "" {
+		return errors.New("execution mode is required for Terraform Cloud workspace")
+	}
+
+	if StringVal(c.ExecutionMode) != "remote" && StringVal(c.ExecutionMode) != "agent" {
+		return fmt.Errorf("execution mode '%s' not supported for CTS, use 'remote' or 'agent' instead", *c.ExecutionMode)
+	}
+
+	if StringVal(c.ExecutionMode) == "agent" &&
+		StringVal(c.AgentPoolID) == "" && StringVal(c.AgentPoolName) == "" {
+		return errors.New("agent_pool_id or agent_pool_name is required if execution mode is 'agent'")
+	}
+
+	if StringVal(c.ExecutionMode) != "agent" &&
+		(StringVal(c.AgentPoolID) != "" || StringVal(c.AgentPoolName) != "") {
+		return errors.New("agent pool configured when execution mode is not 'agent'")
+	}
+
+	if StringVal(c.AgentPoolID) != "" && StringVal(c.AgentPoolName) != "" {
+		logger := logging.Global().Named(logSystemName)
+		logger.Warn("agent_pool_id and agent_pool_name are both configured, agent_pool_id will be used")
+	}
+
+	return nil
+}
+
+// GoString defines the printable version of this struct.
+func (c *TerraformCloudWorkspaceConfig) GoString() string {
+	if c == nil {
+		return "(*TerraformCloudWorkspaceConfig)(nil)"
+	}
+
+	return fmt.Sprintf("&TerraformCloudWorkspaceConfig{"+
+		"AgentPoolID:%s, "+
+		"AgentPoolName:%s,"+
+		"ExecutionMode:%s"+
+		"}",
+		StringVal(c.AgentPoolID),
+		StringVal(c.AgentPoolName),
+		StringVal(c.ExecutionMode),
+	)
+}
---
+++ config/terraform_cloud_workspace_test.go
@@ -0,0 +1,316 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTerraformCloudWorkspaceConfig_Copy(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name string
+		c    *TerraformCloudWorkspaceConfig
+	}{
+		{
+			"nil",
+			nil,
+		},
+		{
+			"empty",
+			&TerraformCloudWorkspaceConfig{},
+		},
+		{
+			"fully_configured",
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("agent"),
+				AgentPoolID:   String("apool-123"),
+				AgentPoolName: String("test_agent_pool"),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := tc.c.Copy()
+			assert.Equal(t, tc.c, c)
+		})
+	}
+}
+func TestTerraformCloudWorkspaceConfig_Merge(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		a    *TerraformCloudWorkspaceConfig
+		b    *TerraformCloudWorkspaceConfig
+		r    *TerraformCloudWorkspaceConfig
+	}{
+		{
+			"nil_a",
+			nil,
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{},
+		},
+		{
+			"nil_b",
+			&TerraformCloudWorkspaceConfig{},
+			nil,
+			&TerraformCloudWorkspaceConfig{},
+		},
+		{
+			"nil_both",
+			nil,
+			nil,
+			nil,
+		},
+		{
+			"empty",
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{},
+		},
+		{
+			"execution_mode_overrides",
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("remote")},
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("agent")},
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("agent")},
+		},
+		{
+			"execution_mode_empty_a",
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("agent")},
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("agent")},
+		},
+		{
+			"execution_mode_empty_b",
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("agent")},
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{ExecutionMode: String("agent")},
+		},
+		{
+			"agent_pool_id_overrides",
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-1")},
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-2")},
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-2")},
+		},
+		{
+			"agent_pool_id_empty_a",
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-2")},
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-2")},
+		},
+		{
+			"agent_pool_id_empty_b",
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-1")},
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{AgentPoolID: String("apool-1")},
+		},
+		{
+			"agent_pool_name_overrides",
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_a")},
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_b")},
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_b")},
+		},
+		{
+			"agent_pool_name_empty_a",
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_b")},
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_b")},
+		},
+		{
+			"agent_pool_name_empty_b",
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_a")},
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{AgentPoolName: String("agent_pool_a")},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := tc.a.Merge(tc.b)
+			assert.Equal(t, tc.r, r)
+		})
+	}
+}
+
+func TestTerraformCloudWorkspaceConfig_Finalize(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		c    *TerraformCloudWorkspaceConfig
+		r    *TerraformCloudWorkspaceConfig
+	}{
+		{
+			"nil",
+			nil,
+			nil,
+		},
+		{
+			"empty",
+			&TerraformCloudWorkspaceConfig{},
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+				AgentPoolID:   String(""),
+				AgentPoolName: String(""),
+			},
+		},
+		{
+			"fully_configured",
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+				AgentPoolID:   String("apool-1"),
+				AgentPoolName: String("test")},
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+				AgentPoolID:   String("apool-1"),
+				AgentPoolName: String("test"),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.c.Finalize()
+			assert.Equal(t, tc.r, tc.c)
+		})
+	}
+}
+
+func TestTerraformCloudWorkspaceConfig_Validate(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name      string
+		expectErr bool
+		c         *TerraformCloudWorkspaceConfig
+	}{
+		{
+			"nil",
+			false,
+			nil,
+		},
+		{
+			"valid_remote",
+			false,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+			},
+		},
+		{
+			"valid_agent_pool_id",
+			false,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("agent"),
+				AgentPoolID:   String("apool-1"),
+			},
+		},
+		{
+			"valid_agent_pool_name",
+			false,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("agent"),
+				AgentPoolName: String("test_agent_pool"),
+			},
+		},
+		{
+			"valid_both_agent_pool_id_and_name",
+			false, // warns but does not error
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("agent"),
+				AgentPoolID:   String("apool-1"),
+				AgentPoolName: String("test_agent_pool"),
+			},
+		},
+		{
+			"no_execution_mode",
+			true,
+			&TerraformCloudWorkspaceConfig{},
+		},
+		{
+			"invalid_execution_mode",
+			true,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("local"),
+			},
+		},
+		{
+			"agent_with_no_agent_pool",
+			true,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("agent"),
+			},
+		},
+		{
+			"remote_with_agent_pool_id",
+			true,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+				AgentPoolID:   String("apool-1"),
+			},
+		},
+		{
+			"remote_with_agent_pool_name",
+			true,
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+				AgentPoolName: String("test_agent_pool"),
+			},
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.c.Validate()
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestTerraformCloudWorkspaceConfig_GoString(t *testing.T) {
+	cases := []struct {
+		name     string
+		c        *TerraformCloudWorkspaceConfig
+		expected string
+	}{
+		{
+			"nil",
+			nil,
+			"(*TerraformCloudWorkspaceConfig)(nil)",
+		},
+		{
+			"remote",
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("remote"),
+			},
+			"&TerraformCloudWorkspaceConfig{" +
+				"AgentPoolID:, " +
+				"AgentPoolName:," +
+				"ExecutionMode:remote" +
+				"}",
+		},
+		{
+			"fully_configured",
+			&TerraformCloudWorkspaceConfig{
+				ExecutionMode: String("agent"),
+				AgentPoolID:   String("apool-1"),
+				AgentPoolName: String("test_agent_pool"),
+			},
+			"&TerraformCloudWorkspaceConfig{" +
+				"AgentPoolID:apool-1, " +
+				"AgentPoolName:test_agent_pool," +
+				"ExecutionMode:agent" +
+				"}",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := tc.c.GoString()
+			assert.Equal(t, tc.expected, r)
+		})
+	}
+}
