020cdd01d23dbcc2e94322c6365c93959d29b487
Lorna Song <lorna@hashicorp.com>
2022-04-13 13:13:13 -0400

    Add placeholder condition monitor struct

---
+++ controller/condition_monitor.go
@@ -0,0 +1,8 @@
+package controller
+
+// ConditionMonitor monitors the the conditions for all of the tasks and is
+// responsible for triggering a task to execute. It uses the task manager to
+// inform of starting / stopping task monitoring as well as executing a task
+type ConditionMonitor struct {
+	// TODO: placeholder. Will convert ReadWrite methods to ConditionMonitor
+}
