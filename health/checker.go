f7195b3da05a78c003590a4032510ffc38e1312c
Michael Wilkerson <mwilkerson@hashicorp.com>
2022-04-28 15:53:29 -0700

    added health checker - health checker is the business logic for determining a healthy system - BasicHealth checker added which just returns nil as there are no additional checks, in the future could be different kind of health checkers

---
+++ health/checker.go
@@ -0,0 +1,37 @@
+package health
+
+import "fmt"
+
+//go:generate mockery --name=Manager --filename=manager.go --output=../mocks/health
+
+// UnhealthySystemError represents an error returned
+// if the system is not healthy. This error can be returned from a health manager
+// to indicate that the response code returned should be a `503`
+type UnhealthySystemError struct {
+	Err error
+}
+
+// Error returns an error string
+func (e *UnhealthySystemError) Error() string {
+	return fmt.Sprintf("CTS is not healthy: %v", e.Err)
+}
+
+// Unwrap returns the underlying error
+func (e *UnhealthySystemError) Unwrap() error {
+	return e.Err
+}
+
+// Checker includes methods necessary for managing and determining the health of the system
+type Checker interface {
+	Check() error
+}
+
+var _ Checker = (*BasicChecker)(nil)
+
+// BasicChecker supports a simple health check, which always returns nil
+type BasicChecker struct{}
+
+// Check always returns nil
+func (h *BasicChecker) Check() error {
+	return nil
+}
---
+++ health/checker_test.go
@@ -0,0 +1,42 @@
+package health
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_BasicHealth_Check(t *testing.T) {
+	t.Parallel()
+
+	h := &BasicChecker{}
+	assert.NoError(t, h.Check())
+}
+
+func TestUnhealthySystemError_Error(t *testing.T) {
+	err := UnhealthySystemError{Err: errors.New("some error")}
+	var nonEnterpriseConsulError *UnhealthySystemError
+
+	assert.True(t, errors.As(&err, &nonEnterpriseConsulError))
+	assert.Equal(t, "CTS is not healthy: some error", err.Error())
+}
+
+func TestUnhealthySystemError_Unwrap(t *testing.T) {
+	var terr *testError
+
+	var otherErr testError
+	err := UnhealthySystemError{Err: &otherErr}
+
+	// Assert that the wrapped error is still detectable
+	// errors.As is the preferred way to call the underlying Unwrap
+	assert.True(t, errors.As(&err, &terr))
+}
+
+type testError struct {
+}
+
+// Error returns an error string
+func (e *testError) Error() string {
+	return "this is a test error"
+}
