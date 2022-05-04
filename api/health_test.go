9aea60d770d2b39bcc18b4495bd173cb5e46dfe1
AJ Jwair <aj.jwair@hashicorp.com>
2022-05-04 13:32:00 -0500

    Added request ID to health check tests Simplified health check tests

--- api/health_test.go
+++ api/health_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"errors"
+	"github.com/google/uuid"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,68 +17,55 @@ import (
 
 func Test_HealthHandler_GetHealth(t *testing.T) {
 	t.Parallel()
+	reqID := uuid.NewString()
 
 	cases := []struct {
-		name     string
-		setup    func(m *mockHealth.Checker)
-		validate func(t *testing.T, resp *httptest.ResponseRecorder)
+		name        string
+		checkReturn error
+		statusCode  int
 	}{
 		{
-			name: "success",
-			setup: func(m *mockHealth.Checker) {
-				m.On("Check").Return(nil)
-			},
-			validate: func(t *testing.T, resp *httptest.ResponseRecorder) {
-				assert.Equal(t, http.StatusOK, resp.Code)
-
-				var hcr oapigen.HealthCheckResponse
-				err := json.NewDecoder(resp.Body).Decode(&hcr)
-				assert.NoError(t, err)
-			},
+			name:        "success",
+			checkReturn: nil,
+			statusCode:  http.StatusOK,
 		},
 		{
-			name: "failure checking health",
-			setup: func(m *mockHealth.Checker) {
-				m.On("Check").Return(errors.New("test error"))
-			},
-			validate: func(t *testing.T, resp *httptest.ResponseRecorder) {
-				assert.Equal(t, http.StatusInternalServerError, resp.Code)
-
-				var hcr oapigen.HealthCheckResponse
-				err := json.NewDecoder(resp.Body).Decode(&hcr)
-				assert.NoError(t, err)
-				assert.Equal(t, "test error", hcr.Error.Message)
-			},
+			name:        "failure checking health",
+			checkReturn: errors.New("test error"),
+			statusCode:  http.StatusInternalServerError,
 		},
 		{
-			name: "failure unhealthy system",
-			setup: func(m *mockHealth.Checker) {
-				m.On("Check").Return(&health.UnhealthySystemError{Err: errors.New("test error")})
-			},
-			validate: func(t *testing.T, resp *httptest.ResponseRecorder) {
-				assert.Equal(t, http.StatusServiceUnavailable, resp.Code)
-
-				var hcr oapigen.HealthCheckResponse
-				err := json.NewDecoder(resp.Body).Decode(&hcr)
-				assert.NoError(t, err)
-				assert.Equal(t, "CTS is not healthy: test error", hcr.Error.Message)
-			},
+			name:        "failure unhealthy system",
+			checkReturn: &health.UnhealthySystemError{Err: errors.New("test error")},
+			statusCode:  http.StatusServiceUnavailable,
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			checker := new(mockHealth.Checker)
-			tc.setup(checker)
+			checker.On("Check").Return(tc.checkReturn)
 			handler := NewHealthHandler(checker)
 
 			path := "/v1/health"
 			req, err := http.NewRequest(http.MethodGet, path, nil)
 			require.NoError(t, err)
-			resp := httptest.NewRecorder()
 
-			handler.GetHealth(resp, req)
-			tc.validate(t, resp)
+			req = req.WithContext(requestIDWithContext(req.Context(), reqID))
+			rr := httptest.NewRecorder()
+
+			handler.GetHealth(rr, req)
+
+			var hcr oapigen.HealthCheckResponse
+			err = json.NewDecoder(rr.Body).Decode(&hcr)
+			assert.NoError(t, err)
+
+			assert.Equal(t, rr.Code, tc.statusCode)
+			assert.Equal(t, hcr.RequestId, oapigen.RequestID(reqID))
+			if tc.checkReturn != nil {
+				assert.NotNil(t, hcr.Error)
+				assert.NotEmpty(t, hcr.Error.Message)
+			}
 		})
 	}
 }
