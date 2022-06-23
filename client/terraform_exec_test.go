23c46786bf8d3f0a7b6dcd93705d7ccf7cfb2289
Lorna Song <lorna@hashicorp.com>
2022-06-23 14:04:58 -0400

    Add Test_TerraformExec_Leak to check for leaked goroutines in tf-exec
    Test does a goroutine check before and after tf-exec is called. Expect this
    test to fail because this issue will be resolved in next commit with a version
    upgrade
    
    Failure:
    
    ```
    === RUN   Test_TerraformExec_Leak
        /Users/lornasong/go/src/github.com/hashicorp/consul-terraform-sync/client/terraform_exec_test.go:60:
            	Error Trace:	terraform_exec_test.go:60
            	Error:      	Not equal:
            	            	expected: 2
            	            	actual  : 6
            	Test:       	Test_TerraformExec_Leak
            	Messages:   	the number of goroutines after the terraform requests should be the same as before
    --- FAIL: Test_TerraformExec_Leak (1.36s)
    ```

---
+++ client/terraform_exec_test.go
@@ -0,0 +1,62 @@
+//go:build integration
+// +build integration
+
+package client
+
+import (
+	"context"
+	"os"
+	"runtime"
+	"testing"
+
+	goVersion "github.com/hashicorp/go-version"
+	hcinstall "github.com/hashicorp/hc-install"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/hc-install/src"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Test_TerraformCLI_Leak checks for an issue with leaked goroutines in
+// terraform-exec <v0.17
+func Test_TerraformExec_Leak(t *testing.T) {
+	// Do not run in parallel with other tasks because this test checks the
+	// number of goroutines
+
+	v, err := goVersion.NewVersion("1.1.8")
+	require.NoError(t, err)
+
+	// Download Terraform binary
+	ctx := context.Background()
+	installer := hcinstall.NewInstaller()
+	_, err = installer.Ensure(ctx, []src.Source{
+		&releases.ExactVersion{
+			Product:    product.Terraform,
+			Version:    v,
+			InstallDir: ".",
+		},
+	})
+	require.NoError(t, err)
+
+	// Cleanup Terraform binary
+	defer func() {
+		_ = os.Remove("terraform")
+	}()
+
+	tfClient, err := NewTerraformCLI(&TerraformCLIConfig{
+		ExecPath:   ".",
+		WorkingDir: ".",
+	})
+	require.NoError(t, err)
+
+	before := runtime.NumGoroutine()
+
+	// Mimics CTS calling terraform
+	tfClient.Init(ctx)
+	tfClient.Apply(ctx)
+
+	after := runtime.NumGoroutine()
+	assert.Equal(t, before, after, "the number of goroutines after the terraform "+
+		"requests should be the same as before")
+}
