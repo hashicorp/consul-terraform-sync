bbd0489038cd8927ef5ec028ba4d0b68a0e194c7
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-10-26 20:39:34 +0530

    release 26 oct

--- version/version.go
+++ version/version.go
@@ -24,12 +24,12 @@ var (
 	//
 	// Version must conform to the format expected by
 	// github.com/hashicorp/go-version for tests to work.
-	Version = "0.7.0"
+	Version = "0.7.1"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 
 	VersionMetadata = ""
 )
