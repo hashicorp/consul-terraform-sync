ba0c913612c7c3f23b79dd46a749de951b1b9936
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-10-27 13:42:13 +0530

    increate time .sleep

--- e2e/condition_schedule_test.go
+++ e2e/condition_schedule_test.go
@@ -121,7 +121,7 @@ func TestCondition_Schedule_Basic(t *testing.T) {
 
 			port := cts.Port()
 			taskSchedule := 10 * time.Second
-			scheduledWait := taskSchedule + 7*time.Second // buffer for task to execute
+			scheduledWait := taskSchedule + 57*time.Second // buffer for task to execute
 
 			// 0. Confirm at least one event for once-mode
 			eventCountBase := eventCount(t, taskName, port)
